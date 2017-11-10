4955c15c4b39d10aa5b0b6042a7ae86f15b5873d
sagazzi <stefano.agazzi@innofis.com>
2017-11-10 10:19:28 +0100

    Changes to be committed: 	modified:   molecule/default/tests/openscap_latest_versions.py

--- molecule/default/tests/openscap_latest_versions.py
+++ molecule/default/tests/openscap_latest_versions.py
@@ -1,4 +1,29 @@
 import sqlite3
+import requests
+import ConfigParser
+
+print('Beginning file download with requests')
+
+url = 'https://copr.fedorainfracloud.org/coprs/openscapmaint/openscap-latest/repo/' \
+      'epel-7/openscapmaint-openscap-latest-epel-7.repo'
+r = requests.get(url)
+
+with open('primary.sqlite', 'wb') as f:
+    f.write(r.content)
+
+# Retrieve HTTP meta-data
+print(r.status_code)
+print(r.headers['content-type'])
+print(r.encoding)
+
+config = ConfigParser.ConfigParser()
+config.read('primary.sqlite')
+
+base_url = config.get('openscapmaint-openscap-latest', 'baseurl')
+
+arch_base_url = base_url.replace("$basearch", "x86_64")
+
+print arch_base_url
 
 conn = sqlite3.connect('primary.sqlite')
 
